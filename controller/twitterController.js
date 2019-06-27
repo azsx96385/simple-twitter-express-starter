44b144054e87d83ff2c161f45086f0d5bbea4921
Shang <vuvu0130@gmail.com>
2019-06-27 10:25:13 +0800

    homepage's top users

--- controller/twitterController.js
+++ controller/twitterController.js
@@ -8,12 +8,22 @@ const twitterController = {
   getTwitters: (req, res) => {
     //看到所有的推文
     Tweet.findAll({ include: User }).then(tweets => {
-      return res.render('tweets', { tweets: tweets })
+      //TOP10 Followings users
+      return User.findAll({ include: [{ model: User, as: "Followers" }] }).then(users => {
+        //重設定users集合，賦予followerCount 跟 isFollowed
+        users = users.map(user => ({
+          ...user.dataValues,
+          FolloweCount: user.Followers.length,
 
+        }))
+        //依followerCount sort users
+        users = users.sort((a, b) => b.FolloweCount - a.FolloweCount)
+        //取前十名
+        users = users.slice(0, 10)
+        return res.render('tweets', { tweets: tweets, users: users })
+      }
+      )
     })
-    //TOP10 Followings users
-
-
   },
   //create 新貼文
   postTwitters: (req, res) => {
@@ -31,4 +41,5 @@ const twitterController = {
     return res.render('tweets')
   }
 }
-module.exports = twitterController 
+module.exports = twitterController
+
