47d67ea1786bc37e1a33fa91aca8281da7b69ddc
Wayne <azsx96385@gmail.com>
2019-06-27 16:21:10 +0800

    user model | set sttribute

--- models/user.js
+++ models/user.js
@@ -1,8 +1,20 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-  }, {});
+  const User = sequelize.define(
+    "User",
+    {
+      //設定欄位屬性
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      name: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      introduction: DataTypes.STRING,
+      role: DataTypes.STRING
+    },
+    {}
+  );
   User.associate = function(models) {
+    //設定model 關聯
   };
   return User;
 };
