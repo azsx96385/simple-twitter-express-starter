c0a53104b45c9180e606a31227365431ef126a86
Wayne <azsx96385@gmail.com>
2019-06-28 11:26:31 +0800

    feature | login logout done

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,23 +19,30 @@ const userController = {
       req.flash("error_messages", "錯誤訊息|資料漏填");
       return res.redirect("back");
     }
+    User.findOne({ where: { name: name } }).then(user => {
+      if (user) {
+        req.flash("error_messages", "錯誤訊息|名稱已被使用");
+        return res.redirect("back");
+      } else {
+        if (password == password_confirm) {
+          //通過-寫入資料庫
+          password = bcrypt.hashSync(
+            req.body.password,
+            bcrypt.genSaltSync(10),
+            null
+          );
+          User.create({ email, name, password }).then(user => {
+            return res.redirect("/users/login");
+          });
+        } else {
+          //驗證失敗
+          console.log("錯誤訊息|密碼輸入不相同");
+          req.flash("error_messages", "錯誤訊息|密碼輸入不相同");
+          return res.redirect("back");
+        }
+      }
+    });
     //驗證密碼相同
-    if (password == password_confirm) {
-      //通過-寫入資料庫
-      password = bcrypt.hashSync(
-        req.body.password,
-        bcrypt.genSaltSync(10),
-        null
-      );
-      User.create({ email, name, password }).then(user => {
-        return res.redirect("/users/login");
-      });
-    } else {
-      //驗證失敗
-      console.log("錯誤訊息|密碼輸入不相同");
-      req.flash("error_messages", "錯誤訊息|密碼輸入不相同");
-      return res.redirect("back");
-    }
   },
   logInPage: (req, res) => {
     return res.render("user_logIn");
@@ -45,7 +52,11 @@ const userController = {
     req.flash("success_messages", "成功訊息|你已經成功登入");
     res.send("你登入拉!!!!");
   },
-  logOut: (req, res) => {}
+  logOut: (req, res) => {
+    req.flash("success_messages", "成功訊息|你已經成功登出");
+    req.logout();
+    res.redirect("/users/logIn");
+  }
 };
 
 //匯出controller
