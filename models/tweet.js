2aa749985c58d328cb88cff8a56a080ee11a7499
Wayne <azsx96385@gmail.com>
2019-06-28 11:44:33 +0800

    modelset |followship-like-reply-tweet |attributr

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,13 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Tweet = sequelize.define('Tweet', {
-  }, {});
-  Tweet.associate = function(models) {
-  };
+  const Tweet = sequelize.define(
+    "Tweet",
+    {
+      description: DataTypes.STRING,
+      UserId: DataTypes.INTEGER
+    },
+    {}
+  );
+  Tweet.associate = function(models) {};
   return Tweet;
 };
