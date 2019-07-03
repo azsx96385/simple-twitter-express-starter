8e90618db5180c28f6546c8d6ef58a9e193ab7f4
Wayne <azsx96385@gmail.com>
2019-07-03 16:09:02 +0800

    feature| admin-tweets filter / navpage

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -9,13 +9,53 @@ const Tweet = db.Tweet;
 const adminController = {
   //推文總攬
   tweetsPage: (req, res) => {
-    Tweet.findAll({ include: [User] }).then(tweets => {
-      console.log(tweets[0]);
-      tweets = tweets.map(tweet => ({
+    //總頁數|上頁|下頁|每頁資料 | 初始值
+    let offset = 0;
+    let limit = 10;
+    //設定偏移量
+    if (req.query.page) {
+      //請求的頁數
+      offset = (req.query.page - 1) * limit;
+    }
+    //開始調用資料 | 指定 偏移量 + limit
+    Tweet.findAndCountAll({
+      include: [User],
+      limit: limit,
+      offset: offset
+    }).then(tweets => {
+      //取得當前頁碼
+      let page = Number(req.query.page) || 1;
+      //取得總頁碼
+      let pages = Math.ceil(tweets.count / limit);
+      //製造頁碼陣列
+      let totalPage = Array.from({ length: pages }).map(
+        (item, index) => index + 1
+      );
+      //計算前頁|次頁頁碼
+      let pre = page - 1 < 1 ? page : page - 1;
+      let next = page + 1 > pages ? pages : page + 1;
+      tweets = tweets.rows.map(tweet => ({
         ...tweet.dataValues,
         description: tweet.dataValues.description.substring(0, 50)
       }));
-      res.render("admin_tweets", { tweets });
+
+      res.render("admin_tweets", { tweets, pre, next, totalPage, page });
+    });
+  },
+  getSearchUser: (req, res) => {
+    let userName = req.body.name;
+    User.findOne({ where: { name: userName } }).then(user => {
+      Tweet.findAll({
+        include: [User],
+        where: { UserId: user.id }
+      }).then(tweets => {
+        tweets = tweets.map(tweet => ({
+          ...tweet.dataValues,
+          description: tweet.dataValues.description.substring(0, 50)
+        }));
+
+        res.render("admin_tweets", { tweets });
+      });
     });
   },
   //推文總攬-刪除推文
@@ -41,13 +81,20 @@ const adminController = {
     }).then(users => {
       //處理description | 僅顯示50字元
       //map + operator + 修改description屬性
+      let userIndex = 1;
       users = users.map(user => ({
         ...user.dataValues,
+        userIndex: userIndex++,
         TweetsAcount: user.userTweets.length,
         FollowingsAcount: user.Followings.length,
         FollowersAcount: user.Followers.length,
         LikesAcount: user.likedTweets.length //
       }));
+
+      //生成user 陣列
+      // let userList=Array.from({length:users.length}).map((item,index)=>
+      //   index+1
+      // )
 
       users = users.sort((a, b) => b.TweetsAcount - a.TweetsAcount);
       res.render("admin_users", { users });
